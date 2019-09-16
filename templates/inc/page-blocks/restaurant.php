8298155a6da1a7885510ad2e63221959e6592396
Your Name <you@example.com>
2019-09-16 13:02:06 +0100

    Restaurant section ready

--- templates/inc/page-blocks/restaurant.php
+++ templates/inc/page-blocks/restaurant.php
@@ -1,6 +1,8 @@
 
 <section id="restaurant">
 <div class="container restaurant">
+<div class="text-center image-sample-container"><img src="https://babs.co.uk/wp-content/uploads/2018/02/7-DS8_2905.jpg" class="img-fluid image-sample" alt="Responsive image"></div>
+
       <div class="col-lg-6 restaurant-text-first">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</div>
       <div class="col-lg-6 restaurant-text-second">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
 </div></section>
