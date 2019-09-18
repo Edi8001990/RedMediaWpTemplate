f83e76ffded2f1c864a7a30d89d4a34b321b3f1f
Your Name <you@example.com>
2019-09-18 15:30:08 +0100

    Instagram frame completed

--- templates/inc/page-blocks/contact.php
+++ templates/inc/page-blocks/contact.php
@@ -10,8 +10,9 @@
             <div><h1 class="contact-header">Lorem ipsum is placeholder text commonlysdsd</h1> </div>
             <div><p class="contact-content">We don’t take bookings so get yourself down to our restaurant and we’ll set you up with a table. </p></div>
         </div>
-
+        <div class="col-md-5">
                 <div class="row">
+                    
                         <table class="contact-table">
 									<tr>
 										<td><i class="fa fa-map-marker"></i></td>
@@ -32,8 +33,10 @@
                                 </table>
                                 
                         <div class="map-container">
+                        
 							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21304.34519112661!2d-4.281123284078983!3d55.85670755739757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48884672d1dc3aeb%3A0x7d210c9b989d4991!2s150%20Edmiston%20Dr%2C%20Glasgow%20G51%202XD!5e0!3m2!1spl!2suk!4v1568813215035!5m2!1spl!2suk" width="450" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-						</div>
+                        </div>
+                        </div>
                          </div>
        
                     </div>
@@ -43,8 +46,9 @@
 
 
       <div class="col-lg-6 contact-block-second">
-          <img src="https://babs.co.uk/wp-content/uploads/2018/02/64-DS7_2168.jpg" class="img-fluid image-menus-sample" alt="Responsive image"></div>
+         <div class="col-md-6 inst-section">
           <?php echo do_shortcode('[instagram-feed]'); ?>
+        </div>
 </div>
 </section>
 
