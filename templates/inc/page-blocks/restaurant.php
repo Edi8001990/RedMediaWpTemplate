c24d887b077210bd4dc438d2f978eeb150191e2d
Your Name <you@example.com>
2019-09-22 13:02:51 +0100

    Continuing plaing with carousel

--- templates/inc/page-blocks/restaurant.php
+++ templates/inc/page-blocks/restaurant.php
@@ -1,9 +1,10 @@
 
 <section id="restaurant">
+
 <div class="row container restaurant">
            
-<div class="col-sm-1"></div>
-                        <div class="col-sm-8 col-xs-10">
+
+                        <!-- <div class="col-sm-8 col-xs-10"> -->
                         <div id="aboutCarousel" class="carousel slide" data-ride="carousel">
 
                         <div class="carousel-inner" role="listbox">
@@ -17,8 +18,8 @@
                                     
                         </div>
                         </div>
-                        </div>
-                        <div class="col-sm-1"></div>                         
+                        <!-- </div> -->
+                                             
       <div class="col-lg-6 restaurant-text-first">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</div>
       <div class="col-lg-6 restaurant-text-second">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
 </div></section>
